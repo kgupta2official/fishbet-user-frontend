1a53916246b295bd6e89df5a04e91ff11dab4de7
DESKTOP-ORJ4M3I\Admin <ketul@pervadorit.com>
2025-04-16 18:54:04 +0530

    resolve compile issue

--- src/components/Header/components/Logo.jsx
+++ src/components/Header/components/Logo.jsx
@@ -1,6 +1,6 @@
 'use client';
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Image from 'next/image';
 import { getSiteDetails } from '@/services/getRequests';
 import fishbetlogo from "../../../../public/assets/logoImage.png.png"
