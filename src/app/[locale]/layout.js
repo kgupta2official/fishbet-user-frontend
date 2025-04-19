5e89ad0b521f6797bbd2b353fd2df31311e222d2
DESKTOP-ORJ4M3I\Admin <ketul@pervadorit.com>
2025-04-19 19:38:40 +0530

    issue resolved

--- src/app/[locale]/layout.js
+++ src/app/[locale]/layout.js
@@ -40,7 +40,9 @@ export function generateStaticParams() {
   return i18nConfig.locales.map((locale) => ({ locale }));
 }
 
-export default async function RootLayout({ children, params: { locale } }) {
+// export default async function RootLayout({ children, params: { locale } }) {
+export default async function RootLayout({ children, params}) {
+  const { locale } = await params;
   const { resources } = await initTranslations(locale, i18nNamespaces);
 
   return (
