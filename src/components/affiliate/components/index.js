c8436d4cce9c6a19c20298c33f1838daff8dc01d
DESKTOP-ORJ4M3I\Admin <ketul@pervadorit.com>
2025-04-26 01:46:48 +0530

    s

--- src/components/affiliate/components/index.js
+++ src/components/affiliate/components/index.js
@@ -20,27 +20,25 @@ const Affiliate = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
   const active = searchParams.get('active');
-  const [activeTab, setActiveTab] = useState(active || TAB_CONTROLS[0].value); // Default to first tab if 'active' is null
+  const [activeTab, setActiveTab] = useState(active || TAB_CONTROLS[0].value);
 
   const onClose = () => {
-    router.push('/'); // Close affiliate panel and redirect to homepage
+    router.push('/');
   };
 
   const onTabChange = (value) => {
-    router.push(`/affiliate/?active=${value}`); // Update URL on tab change
-    setActiveTab(value); // Update active tab in state
+    router.push(`/affiliate/?active=${value}`);
+    setActiveTab(value);
   };
 
   useEffect(() => {
     if (!active) {
-      onTabChange(TAB_CONTROLS[0].value); // If no 'active' query param, default to first tab
+      onTabChange(TAB_CONTROLS[0].value);
     }
-  }, [active]); // Runs whenever 'active' query param changes
-
-  const Component = COMPONENT_MAPPING[activeTab]; // Dynamically select the component based on active tab
-
+  }, [active]);
+  const Component = COMPONENT_MAPPING[activeTab];
   return (
-    <div className={`${style.wrapContainer} bg-[hsl(var(--main-background))]`}>
+    <div className={`${style.wrapContainer} bg-[hsl(var(--main-background))] `}>
       <section>
         <div className="flex justify-between mb-4">
           <div className="text-white box-border font-montserrat text-[20px] font-extrabold">
@@ -57,7 +55,7 @@ const Affiliate = () => {
             onValueChange={onTabChange}
           />
         </div>
-        <div className="flex gap-5 xl:grid-flow-col items-start">
+        <div className="flex gap-5 xl:grid-flow-col items-start ">
           <div className="hidden xl:block">
             <PanelTabs
               activeTab={activeTab}
@@ -65,8 +63,10 @@ const Affiliate = () => {
               tabControls={TAB_CONTROLS}
             />
           </div>
-          <div className="bg-[rgb(var(--lb-blue-900))] p-6 xl:tw-p-6 rounded overflow-hidden w-full">
-            {/* Dynamically render the active component */}
+          <div className="bg-[rgb(var(--lb-blue-900))] p-6 xl:tw-p-6  rounded overflow-hidden w-full">
+            {/* {active === 'getStart' ? <Start /> : null}
+            {active === 'referrals' ? <Referrals /> : null}
+            {active === 'funds' ? <Funds /> : null} */}
             <Component />
           </div>
         </div>
@@ -74,5 +74,4 @@ const Affiliate = () => {
     </div>
   );
 };
-
 export default Affiliate;
