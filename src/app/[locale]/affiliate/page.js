38d25b2866210e479deea32fc6d3f5cca49532ee
DESKTOP-ORJ4M3I\Admin <ketul@pervadorit.com>
2025-04-26 01:22:50 +0530

    dynamic

--- src/app/[locale]/affiliate/page.js
+++ src/app/[locale]/affiliate/page.js
@@ -1,6 +1,8 @@
-
 import dynamic from 'next/dynamic';
-const Affiliate = dynamic(() => import('@/components/affiliate/components'));
+
+const Affiliate = dynamic(() => import('@/components/affiliate/components'), {
+  ssr: false,
+});
 
 export default function Page() {
   return <Affiliate />;
